650ab1450598bad06c6ba3cbc32c3e5dabd5b9a1
abdrahmanES1 <68553016+abdrahmanES1@users.noreply.github.com>
2021-07-06 23:20:47 +0100

    add scoll to Top button

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,8 @@
 let emailInput = document.getElementById('email-input'),
     subjectInput = document.getElementById('subject-input'),
     textArea = document.getElementById('message-input'),
-    contactBtn = document.querySelector('button');
+    contactBtn = document.querySelector('button'),
+    scrollToTopBtn = document.querySelector('.scrollToTop');
     
 emailInput.onfocus = () => emailInput.placeholder = '';
 emailInput.onblur = () => emailInput.placeholder = 'Your email';   
@@ -16,7 +17,16 @@ textArea.onblur = function(){
     this.textContent = 'Message here...';
 };
 
-contactBtn.onclick = ()=> window.scrollTo(100, 10000);
+contactBtn.onclick = () => window.scrollTo(100, 10000);
 
 
+document.body.onscroll = _ =>
+{
+    if (window.scrollY > 500) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
 
+scrollToTopBtn.onclick = _=>window.scrollTo(0,0);
